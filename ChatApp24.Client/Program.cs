66a94243d77191e1ebdb24e75c83c31cd91374ce
peshopbs2 <peshopbs2@gmail.com>
2024-12-07 02:23:11 -0800

    Add project files.

---
+++ ChatApp24.Client/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Reflection.Metadata;
+string hostname = Dns.GetHostName();
+IPHostEntry ipHostInfo = Dns.GetHostEntry(hostname);
+IPAddress ip = ipHostInfo.AddressList[0];
+IPEndPoint endpoint = new IPEndPoint(ip, 11223);
+Socket socket = new Socket(ip.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+try
+{
+    socket.Connect(endpoint);
+    Console.WriteLine($"Connected to {socket.RemoteEndPoint.ToString()}");
+    byte[] buffer = new byte[4096];
+    while (true)
+    {
+        Console.ForegroundColor = ConsoleColor.Blue;
+        string input = Console.ReadLine();
+        byte[] message = Encoding.UTF8.GetBytes(input);
+        socket.Send(message);
+
+        if(input == "exit")
+        {
+            break;
+        }
+
+        int messageSize = socket.Receive(buffer);
+        string response = Encoding.UTF8.GetString(buffer, 0, messageSize);
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(response);
+    }
+
+    socket.Shutdown(SocketShutdown.Both);
+    socket.Close();
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
---
+++ ChatApp24/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+string hostname = Dns.GetHostName();
+IPHostEntry ipHostInfo = Dns.GetHostEntry(hostname);
+IPAddress ip = ipHostInfo.AddressList[0];
+IPEndPoint endpoint = new IPEndPoint(ip, 11223);
+
+Socket socket = new Socket(ip.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+try
+{
+    socket.Bind(endpoint);
+    socket.Listen();
+    Console.WriteLine("Listening for the voices...");
+    Socket handle = socket.Accept();
+    byte[] buffer = new byte[4096];
+    string message = "";
+    while (true)
+    {
+        int messageSize = handle.Receive(buffer);
+        message = Encoding.UTF8.GetString(buffer, 0, messageSize);
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine(message);
+        if (message == "exit")
+        {
+            break;
+        }
+
+        Console.ForegroundColor = ConsoleColor.Green;
+        string input = Console.ReadLine();
+        byte[] data = Encoding.UTF8.GetBytes(input);
+        handle.Send(data);
+    }
+
+    handle.Shutdown(SocketShutdown.Both);
+    handle.Close();
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+    Console.WriteLine("Something went wrong...");
+}
